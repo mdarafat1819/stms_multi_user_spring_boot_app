661bcea2947d1438fd28480d44f67c95834dd73e
Arafat <mdarafat1819@gmail.com>
2026-02-02 08:51:51 +0600

    user Entity added

--- src/main/java/com/example/stms_multi_user/controllers/HomeController.java
+++ src/main/java/com/example/stms_multi_user/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.stms_multi_user.controller;
+package com.example.stms_multi_user.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/example/stms_multi_user/entities/User.java
@@ -0,0 +1,36 @@
+package com.example.stms_multi_user.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter 
+@Setter
+@Table(name = "users")
+public class User extends AuditableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private boolean enabled;
+    
+}
